699b7ac0568024195c1623e2563f93bfd8020d1f
arajem <ara.aboudi12@gmail.com>
2024-11-05 21:17:32 +0100

    Update ara.py

--- ara.py
+++ ara.py
@@ -7,7 +7,6 @@ import plotly.graph_objs as go
 from datetime import datetime, timedelta
 import requests
 import matplotlib.dates as mdates
-from matplotlib import colors 
 
 
 
@@ -154,7 +153,7 @@ with tab4:
     plt.figure(figsize=(10, 6))
     plt.plot(simulations)
     
-    current_price_line = plt.axhline(y=last_price, color='indigo', linewidth=2) 
+    current_price_line = plt.axhline(y=last_price, color='blue', linewidth=2) 
     plt.title(f"{n_simulations} Monte Carlo Simulations for {stock_symbol} over {time_horizon} Days")
     plt.legend([current_price_line], [f'Current stock price: ${last_price:.2f}'])
     plt.xlabel("Day")
@@ -193,7 +192,7 @@ with tab5:
 
     st.write("### Stock Performance Over Time")
     fig_line = go.Figure()
-    fig_line.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Closing Price', fill='darkmagenta', fillcolor='plum'))
+    fig_line.add_trace(go.Scatter(x=data.index, y=data['Close'], mode='lines', name='Closing Price', fill='tozeroy', fillcolor='#66CDAA'))
     fig_line.update_layout(
         title=f"{stock_symbol} - 1 Year Stock Performance",
         xaxis_title="Date",
